36401a907e2cec0327ab7151ec409cb11023ef7c
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-10 22:00:31 +0000

    more security done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # Allow better_errors to work in online IDE
+    # Allow better_errors to work in online IDE
   config.web_console.whitelisted_ips = "0.0.0.0/0.0.0.0"
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0.0.0.0"
   # Auto-connect to database when rails console opens
