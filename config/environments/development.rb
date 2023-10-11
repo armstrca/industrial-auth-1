962ca0c709526a37e83ba5cf96ae25bb4f70a802
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-11 14:25:30 +0000

    got live app preview to work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts.clear
     # Allow better_errors to work in online IDE
   config.web_console.whitelisted_ips = "0.0.0.0/0.0.0.0"
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0.0.0.0"
